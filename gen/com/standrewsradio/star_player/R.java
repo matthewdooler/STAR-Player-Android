59e599f6328ed7fb0a3d1d658207a681b476d0b3
ryoyanagida <ryoyanagida@thinkpadx201tryo>
2014-07-26 23:57:52 +0900

    support for landscape layouts

--- gen/com/standrewsradio/star_player/R.java
+++ gen/com/standrewsradio/star_player/R.java
@@ -24,6 +24,8 @@ public final class R {
     public static final class id {
         public static final int imageButton1=0x7f080001;
         public static final int imageView1=0x7f080000;
+        public static final int linearLayout1=0x7f080002;
+        public static final int webView1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,6 +36,7 @@ public final class R {
     public static final class string {
         public static final int STAR=0x7f050004;
         public static final int app_name=0x7f050000;
+        public static final int buzzbox=0x7f050005;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int play=0x7f050003;
