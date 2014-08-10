a83b45a6d1bff80b0f27b6f2443040b92eb27c7a
ryoyanagida <ryoyanagida@thinkpadx201tryo>
2014-08-10 11:31:36 +0900

    created handler

---
+++ src/com/standrewsradio/star_player/WebViewHandler.java
@@ -0,0 +1,27 @@
+package com.standrewsradio.star_player;
+
+import android.view.View;
+
+public class WebViewHandler {
+	//class to handle webview
+	
+	View view;
+	
+	View webview;
+	
+	boolean online;
+	
+	public void initialisze(){
+		//initialise webview object
+	}
+	public void update(){
+		if(online){
+			//load standrewsradio.com/_buzzbox
+			
+		}else{
+			//change view to display - no connection display
+			
+		}
+	}
+	
+}
