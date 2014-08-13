326bd605f8f2073c61637afa6e6abd4d6d95c900
ryoyanagida <ryoyanagida@thinkpadx201tryo>
2014-08-13 21:47:40 +0900

    writing handler

--- src/com/standrewsradio/star_player/WebViewHandler.java
+++ src/com/standrewsradio/star_player/WebViewHandler.java
@@ -1,22 +1,28 @@
 package com.standrewsradio.star_player;
 
+import java.io.File;
+
 import android.view.View;
+import android.webkit.WebView;
 
 public class WebViewHandler {
+	private static final String NOCONNECTIONFILE = "noconnection.html";
 	//class to handle webview
 	
-	View view;
-	
-	View webview;
+	private final String ENCODING = "UTF-8";
+	private final String HTML = "text/html";
+
+	WebView webview;
 	
 	boolean online;
 	
-	public WebViewHandler(View webview){
+	public WebViewHandler(WebView webview){
 		this.webview = webview;
 	}
 	
 	public void initialisze(){
 		//initialise webview object
+		this.update();
 	}
 	
 	public void update(){
@@ -25,8 +31,20 @@ public class WebViewHandler {
 			
 		}else{
 			//change view to display - no connection display
-			
+			String data = loadHTMLFile(NOCONNECTIONFILE);
+			webview.loadData(data, HTML, ENCODING);
 		}
+	}
+	
+	public String loadHTMLFile(String filename){
+		File htmlFile = new File(filename);
+		String stringHtml = "";
+		if(htmlFile.exists()){
+			
+		}else{
+			return null;
+		}
+		return stringHtml;
 	}
 	
 }
